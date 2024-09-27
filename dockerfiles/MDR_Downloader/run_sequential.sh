b67fe4e787c052c4d312e791b204ca46196c2ee9
Michele Scarlato <michele.scarlato@gmail.com>
2024-09-27 13:25:26 +0200

    Added --remove-orphans to the script.

--- dockerfiles/MDR_Downloader/run_sequential.sh
+++ dockerfiles/MDR_Downloader/run_sequential.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
 echo "Running BioLINCC container"
-docker-compose up biolincc
-docker-compose down
+docker-compose up --remove-orphans biolincc
+docker-compose down --remove-orphans
 
 echo "Running ClinicalTrials.gov container"
-docker-compose up clinicaltrials
-docker-compose down
+docker-compose up --remove-orphans clinicaltrials
+docker-compose down --remove-orphans
 
 echo "Running EUCTR(EMA) container"
-docker-compose up euctr
-docker-compose down
+docker-compose up --remove-orphans euctr
+docker-compose down --remove-orphans
 
 echo "Running ISRCTN container"
-docker-compose up isrctn
-docker-compose down
+docker-compose up --remove-orphans isrctn
+docker-compose down --remove-orphans
 
 echo "Running PubMed1 container"
-docker-compose up pubmed1
-docker-compose down
+docker-compose up --remove-orphans pubmed1
+docker-compose down --remove-orphans
 
 echo "Running PubMed2 container"
-docker-compose up pubmed2
-docker-compose down
+docker-compose up --remove-orphans pubmed2
+docker-compose down --remove-orphans
 
 echo "Running WHO container"
-docker-compose up who
-docker-compose down
+docker-compose up --remove-orphans who
+docker-compose down --remove-orphans
 
 echo "Running Yoda1 container"
-docker-compose up yoda1
-docker-compose down
+docker-compose up --remove-orphans yoda1
+docker-compose down --remove-orphans
 
 echo "Running Yoda2 container"
-docker-compose up yoda2
-docker-compose down
+docker-compose up --remove-orphans yoda2
+docker-compose down --remove-orphans
